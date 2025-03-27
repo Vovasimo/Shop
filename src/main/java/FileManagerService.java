ac82678bab806accc2286766ee89249683acb282
Vovasimo <kizivovan@gmail.com>
2025-03-27 16:08:41 +0200

    Version 1.5 Stable version

--- src/main/java/FileManagerService.java
+++ src/main/java/FileManagerService.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 public class FileManagerService {
 
     public void addObjectToFile(Item item, String fileName) {
-        addObjectToFile(item.getClass().getSimpleName(), item.getId(), item.getName(), item.getPrice(), item.getDescription(), fileName);
+        addObjectToFile(item.getClass().getSimpleName().toLowerCase(), item.getId(), item.getName(), item.getPrice(), item.getDescription(), fileName);
     }
 
     public void addObjectToFile(String type, Integer id, String name, Float price, String description, String fileName) {
@@ -29,6 +29,38 @@ public class FileManagerService {
 
             Files.write(Paths.get(fileName), jsonArray.toString(4).getBytes());
             System.out.println("Item added to file: " + object.toString());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void removeObjectFromFile(Item item, String fileName) {
+        removeObjectFromFile(item.getClass().getSimpleName().toLowerCase(), item.getId(), item.getName(), item.getPrice(), item.getDescription(), fileName);
+    }
+
+    public void removeObjectFromFile(String type, Integer id, String name, Float price, String description, String fileName) {
+        JSONObject object = new JSONObject();
+
+        object.put("type", type);
+        object.put("id", id);
+        object.put("name", name);
+        object.put("price", price);
+        object.put("description", description);
+
+        try {
+            String bytes = new String(Files.readAllBytes(Paths.get(fileName)));
+            JSONArray jsonArray = new JSONArray(bytes);
+
+            jsonArray.put(object);
+            for(int i=0; i!= jsonArray.length();i++){
+                if(jsonArray.get(i).equals(object)){
+                    jsonArray.remove(i);
+                }
+            }
+
+            Files.write(Paths.get(fileName), jsonArray.toString(4).getBytes());
+            System.out.println("Item removed from file: " + object.toString());
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
--- src/main/java/UserInterfaceService.java
+++ src/main/java/UserInterfaceService.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class UserInterfaceService {
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main() {
+        boolean exit = false;
+        welcome();
+
+        while(!exit){
+            actions();
+
+        }
+    }
+
+    public static void welcome() {
+        System.out.println("\n===== Welcome! =====");
+    }
+
+    public static void actions() {
+        System.out.println("\nWhat you want to do?");
+        System.out.println("1. View products");
+        System.out.println("2. Cart");
+        System.out.println("3. Payment");
+        System.out.println("0. Logout");
+        System.out.println("====================");
+    }
+
+    public static void Cart() {
+
+        System.out.println("\nWhat you want to do?");
+        System.out.println("1. View products");
+        System.out.println("2. Cart");
+        System.out.println("3. Payment");
+        System.out.println("0. Logout");
+        System.out.println("====================");
+    }
+
+    public static void remove() {
+
+    }
 }
